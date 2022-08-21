35f43fde84826ba51c6dd0e39ce41b7ad413c59e
sor-ca <mariagrandova@gmail.com>
2022-08-21 17:40:45 +0300

    server chenges port after receiving message

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,22 @@
 use std::net::UdpSocket;
 use std::str::from_utf8;
-use std::time::Duration;
+//use std::time::Duration;
 fn main() {
     let socket = UdpSocket::bind("127.0.0.1:8081").expect("couldn't bind to address");
-    socket.set_read_timeout(Some(Duration::from_secs(1))).unwrap();
+    //socket.set_read_timeout(Some(Duration::from_secs(1))).unwrap();
     let buf = b"Hello, world!";
-    socket.send_to(&buf[..], "127.0.0.1:8080").expect("couldn't send data");
+    //let mut buf = [0u8; 516];
+    //buf = "Hello, world!".as_bytes();
+    socket.send_to(&buf[..], "127.0.0.1:69").expect("couldn't send data");
     let mut r_buf = [0; 516];
     let (number_of_bytes, src_addr) = socket.recv_from(&mut r_buf).expect("didn't receive data");
-    let filled_buf = &mut r_buf[..number_of_bytes];
+    let mut filled_buf = &mut r_buf[..number_of_bytes];
+    dbg!(src_addr);
+    dbg!(from_utf8(&filled_buf).expect("can't read message"));
+    let buf = b"second_message";
+    socket.send_to(&buf[..], src_addr).expect("couldn't send data");
+    let (number_of_bytes, src_addr) = socket.recv_from(&mut r_buf).expect("didn't receive data");
+    filled_buf = &mut r_buf[..number_of_bytes];
     dbg!(src_addr);
     dbg!(from_utf8(&filled_buf).expect("can't read message"));
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,29 @@
 use std::net::UdpSocket;
 use std::str::from_utf8;
 use std::{thread, time};
+//use std::net::SocketAddr;
 
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:8080").expect("couldn't bind to address");
+    let start_socket = UdpSocket::bind("127.0.0.1:69").expect("couldn't bind to address");
+    let mut buf = [0; 516];
+    let (number_of_bytes, src_addr) = start_socket.recv_from(&mut buf).expect("didn't receive data");
+    dbg!(src_addr);
+    let mut filled_buf = &mut buf[..number_of_bytes];
+    dbg!(from_utf8(&filled_buf).expect("can't read message"));
+    let new_socket = UdpSocket::bind("127.0.0.1:8080").expect("couldn't bind to address");
+    new_socket.send_to(&filled_buf, src_addr).expect("couldn't send data");
+
     loop {
-        let mut buf = [0; 516];
-        let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("didn't receive data");
+        //let mut buf = [0; 516];
+        //let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("didn't receive data");
+        let (number_of_bytes, src_addr) = new_socket.recv_from(&mut buf).expect("didn't receive data");
         dbg!(src_addr);
-        let filled_buf = &mut buf[..number_of_bytes];
+        filled_buf = &mut buf[..number_of_bytes];
         dbg!(from_utf8(&filled_buf).expect("can't read message"));
         thread::sleep(time::Duration::from_secs(1));
         //let message_text = from_utf8(&buf[4..]).expect("can't read message");
         //println!("server receives {}", message_text);
-        socket.send_to(&filled_buf, src_addr).expect("couldn't send data");
+        new_socket.send_to(&filled_buf, src_addr).expect("couldn't send data");
     }    
 }
 
