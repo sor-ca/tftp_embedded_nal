5d9432360f5a87ca8c42bb9f61b9434e1dbb9dd1
sor-ca <mariagrandova@gmail.com>
2022-08-21 16:08:52 +0300

    with thread::sleep

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
 use std::net::UdpSocket;
 use std::str::from_utf8;
+use std::{thread, time};
+
 fn main() {
     let socket = UdpSocket::bind("127.0.0.1:8080").expect("couldn't bind to address");
     loop {
@@ -7,7 +9,8 @@ fn main() {
         let (number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("didn't receive data");
         dbg!(src_addr);
         let filled_buf = &mut buf[..number_of_bytes];
-        dbg!(from_utf8(&filled_buf).unwrap());
+        dbg!(from_utf8(&filled_buf).expect("can't read message"));
+        thread::sleep(time::Duration::from_secs(1));
         //let message_text = from_utf8(&buf[4..]).expect("can't read message");
         //println!("server receives {}", message_text);
         socket.send_to(&filled_buf, src_addr).expect("couldn't send data");
