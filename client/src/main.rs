a6d61dd7d7c0be94c5375d00481ed8f6dd106869
sor-ca <mariagrandova@gmail.com>
2022-08-22 12:21:05 +0300

    sending data from file and ack

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,29 +6,54 @@ use message::Message;
 use ascii::AsciiStr;
 extern crate alloc;
 use alloc::vec::Vec;
-//use std::time::Duration;
+use std::time::Duration;
 fn main() {
     let socket = UdpSocket::bind("127.0.0.1:8081").expect("couldn't bind to address");
-    //socket.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
+    socket.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
 
     let path = "read_from.txt";
 
     let packet: Vec<u8> = Message::wrq(AsciiStr::from_ascii(path.as_bytes()).unwrap(), true).unwrap().into();
-    
     socket.send_to(packet.as_slice(), "127.0.0.1:69").expect("couldn't send data");
-    let mut r_buf = [0; 516];
-    let (number_of_bytes, src_addr) = socket.recv_from(&mut r_buf).expect("didn't receive data");
-    let mut filled_buf = &mut r_buf[..number_of_bytes];
-    dbg!(src_addr);
-    dbg!(from_utf8(&filled_buf).expect("can't read message"));
 
-    let mut buf = [0; 516];
+    let mut r_buf = [0; 516];
+    //necessary to add break after several error messages
+    loop {
+        let (_number_of_bytes, src_addr) = socket.recv_from(&mut r_buf).expect("didn't receive data");
+        let message = Message::try_from(&r_buf[..]).expect("can't convert buf to message");
+        match message {
+            Message::Ack(0) => {
+                println!("receive ack message");
+                socket.connect(src_addr).expect("connect function failed");
+                break;
+            },
+
+            _ => continue,
+        } 
+    }
+
+    let mut buf = [0; 512];
     let mut f = File::open(path).unwrap();
     f.read(&mut buf).unwrap();
+    let mut block_id = 1u16;
+    let packet: Vec<u8> = Message::data(block_id, &buf[..]).unwrap().into(); 
     
-    socket.send_to(&buf[..], src_addr).expect("couldn't send data");
-    let (number_of_bytes, src_addr) = socket.recv_from(&mut r_buf).expect("didn't receive data");
-    filled_buf = &mut r_buf[..number_of_bytes];
-    dbg!(src_addr);
-    dbg!(from_utf8(&filled_buf).expect("can't read message"));
+    loop {
+        socket.send(packet.as_slice()).expect("couldn't send data");
+        socket.recv(&mut r_buf).expect("didn't receive data");
+        let message = Message::try_from(&r_buf[..]).expect("can't convert buf to message");
+        match message {
+            Message::Ack(id) => {
+                if id == block_id {
+                    println!("receive ack message");
+                    break;
+                } else {
+                    println!("wrong block id");
+                    continue;
+                };
+                
+            },
+            _ => continue,
+        } 
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -4,39 +4,55 @@ use std::{thread, time};
 use std::io::Write;
 use std::fs::File;
 use message::{Message, FileOperation};
+use ascii::AsciiStr;
+extern crate alloc;
+use alloc::vec::Vec;
+use std::time::Duration;
 //use std::net::SocketAddr;
 
 fn main() {
-    let start_socket = UdpSocket::bind("127.0.0.1:69").expect("couldn't bind to address");
-    //start_socket.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
+    let mut socket = UdpSocket::bind("127.0.0.1:69").expect("couldn't bind to address");
+    socket.set_read_timeout(Some(Duration::from_secs(100))).unwrap();
     let mut buf = [0; 516];
-    let (number_of_bytes, src_addr) = start_socket.recv_from(&mut buf).expect("didn't receive data");
-    dbg!(src_addr);
-    dbg!(&buf);
-    let message = Message::try_from(&buf[..]).expect("can't convert buf to message");
-    match message {
-        Message::File {operation: FileOperation::Write, ..} => println!("receive wrq message"),
-        _ => println!("receive wrong message"),
-    }
-    //let mut filled_buf = &mut buf[..number_of_bytes];
-    let mut f = File::create("write_into.txt").unwrap();
-    //f.write(&mut filled_buf).unwrap();
-    //dbg!(from_utf8(&filled_buf).expect("can't read message"));
-
-    //i don't like to create new socket, but i don't understand how to change port
-    let new_socket = UdpSocket::bind("127.0.0.1:8080").expect("couldn't bind to address");
-    //new_socket.set_read_timeout(Some(Duration::from_secs(10))).unwrap();
-    new_socket.send_to(&mut buf, src_addr).expect("couldn't send data");
-
+    //necessary to add break after several error messages
     loop {
-        let (number_of_bytes, src_addr) = new_socket.recv_from(&mut buf).expect("didn't receive data");
-        dbg!(src_addr);
-        let mut filled_buf = &mut buf[..number_of_bytes];
-        dbg!(from_utf8(&filled_buf).expect("can't read message"));
+        let (_number_of_bytes, src_addr) = socket.recv_from(&mut buf).expect("didn't receive data");
+        let message = Message::try_from(&buf[..]).expect("can't convert buf to message");
+        match message {
+            Message::File {operation: FileOperation::Write, ..} => {
+                println!("receive wrq message");
+                socket = UdpSocket::bind("127.0.0.1:8080").expect("couldn't bind to address");
+                socket.connect(src_addr).expect("connect function failed");
+                let packet: Vec<u8> = Message::ack(0).into();
+                socket.send(packet.as_slice()).expect("couldn't send data");
+                break;
+            },
 
-        f.write(&mut filled_buf).unwrap();
-        thread::sleep(time::Duration::from_secs(1));
-        new_socket.send_to(&filled_buf, src_addr).expect("couldn't send data");
+            _ => continue,
+        }
+    }
+    
+
+    let mut f = File::create("write_into.txt").unwrap();
+
+    //necessary to add break after several error messages
+    loop {
+        socket.recv(&mut buf).expect("didn't receive data");
+        let message = Message::try_from(&buf[..]).expect("can't convert buf to message");
+        match message {
+            Message::Data(block_id, data) => {
+                println!("receive data packet");
+                dbg!(from_utf8(data.as_ref()).expect("can't read message"));
+
+                let packet: Vec<u8> = Message::ack(block_id).into();                
+                f.write(data.as_ref()).unwrap();
+                thread::sleep(time::Duration::from_secs(1));
+                socket.send(packet.as_slice()).expect("couldn't send data");
+                break;
+            },
+
+            _ => continue,
+        }
     }    
 }
 
