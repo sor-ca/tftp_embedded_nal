accececd3f361481c0cd7960ad8ba94d5c72f285
sor-ca <mariagrandova@gmail.com>
2022-09-13 20:25:34 +0300

    cosmetics

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,7 @@
 use std::{
     fs::File,
     io::{Read, Write},
-    //str::from_utf8,
-    //thread,
-    //time::{self, Duration},
 };
-//use tftp::{FileOperation, Message};
 use std_embedded_nal::Stack;
 use server::{TftpServer, RequestType};
 
@@ -40,32 +36,4 @@ fn main() {
         },
 
     }
-    /*let message: tftp::Message = mess[..].try_into().unwrap();
-    match message {
-        Message::File {
-            operation: FileOperation::Write,
-            ..
-        } => match server.write(src_addr) {
-            Ok(vec)  => {
-                let mut f = File::create("file1.txt").unwrap();
-                f.write(vec.as_slice()).unwrap();
-            },
-            Err(_) => println!("server writing error"),
-        },
-        Message::File {
-            operation: FileOperation::Read,
-            path,
-            ..
-        } => {
-            let mut vec: Vec<u8> = vec![];
-            let mut f = File::open(path.as_str()).unwrap();
-            f.read_to_end(&mut vec).unwrap();
-            match server.read(src_addr, &mut vec) {
-                Ok(_)  => (),
-                Err(_) => println!("server reading error"),
-            }
-        },
-        //to satisfy the compiler
-        _ => (),
-    };*/
 }
