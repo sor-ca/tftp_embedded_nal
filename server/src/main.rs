bc33e492697e08dc72b34722f681c5cf289c3c6e
sor-ca <mariagrandova@gmail.com>
2022-09-26 15:50:42 +0300

    try to switch to smoltcp library. Errors

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,14 +2,60 @@ use std::{
     fs::File,
     io::{Read, Write},
 };
-use std_embedded_nal::Stack;
+//use std_embedded_nal::Stack;
+use smoltcp_nal::{UdpSocket, NetworkError, NetworkStack};
 use tftp_embedded_nal::server::{TftpServer, RequestType};
 use heapless::Vec;
+use smoltcp::iface::{InterfaceBuilder, NeighborCache};
+use smoltcp::socket::udp::Socket;
+use std::collections::BTreeMap;
+use smoltcp::phy::{Loopback, Medium};
+use smoltcp::wire::{EthernetAddress, IpCidr, Ipv6Address, IpAddress};
+use embedded_time::{clock::{Clock, Error}, fraction::Fraction, Instant};
+
+struct SomeClock;
+
+impl Clock for SomeClock {
+    type T = u32;
+    const SCALING_FACTOR: Fraction = Fraction::new(1, 16_000_000);
+    fn try_now(&self) -> Result<Instant<Self>, Error> {
+        Ok(Instant::new(100))
+    }
+}
 
 fn main() {
-    let std_stack = Stack::default();
+    //let std_stack = Stack::default();
+    let device = Loopback::new(Medium::Ethernet);
+    let hw_addr = EthernetAddress::default();
+    let neighbor_cache = NeighborCache::new(BTreeMap::new());
+    let ip_addrs = [
+        IpCidr::new(IpAddress::Ipv6(Ipv6Address::LOOPBACK), 64),
+    ];
+
+    //let iface = InterfaceBuilder::new(device, vec![])
+    let iface = InterfaceBuilder::new()
+        .hardware_addr(hw_addr.into())
+        .neighbor_cache(neighbor_cache)
+        .ip_addrs(ip_addrs)
+        .finalize(&mut device);
+
+    let udp_rx_buffer = udp::PacketBuffer::new(
+        vec![udp::PacketMetadata::EMPTY, udp::PacketMetadata::EMPTY],
+        vec![0; 65535],
+    );
+    let udp_tx_buffer = udp::PacketBuffer::new(
+        vec![udp::PacketMetadata::EMPTY, udp::PacketMetadata::EMPTY],
+        vec![0; 65535],
+    );
+    let udp_socket = udp::Socket::new(udp_rx_buffer, udp_tx_buffer);
+    iface.add_socket(udp_socket);
+
+    let clock: SomeClock;
+
+    let stack = NetworkStack::new(iface, clock);
+
     let mut server = TftpServer::new(
-        std_stack,
+        stack,
     );
     println!("create server");
     let (req_type, src_addr, filename) = match server.listen() {
